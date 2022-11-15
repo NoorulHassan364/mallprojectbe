6d99fa144db3f0116177782f9111e9162d047f74
noorulhassan364 <noorulhassan364@gmail.com>
2022-11-15 17:47:22 +0500

    chanegs

--- src/models/shopModel.js
+++ src/models/shopModel.js
@@ -12,10 +12,6 @@ const shopSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  rent: {
-    type: String,
-    required: true,
-  },
   image: {
     type: String,
     required: true,
